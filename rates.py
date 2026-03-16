a70cf6ede2ac48f0d5695365a77002994c5e7c44
imranarain84 <imranarain84@gmail.com>
2026-03-16 16:16:05 -0400

    Create rates.py

---
+++ rates.py
@@ -0,0 +1,23 @@
+# Daily rates extracted from Billing Profile PDF
+STORAGE_RATES = {
+    "Standard Bin": 0.0442,
+    "Blue Bin Small": 0.0488,
+    "Blue Bin Medium": 0.1462,
+    "Blue Bin Large": 0.2925,
+    "Gray Bin Small": 0.1846,
+    "Gray Bin Medium": 0.2275,
+    "Gray Bin Large": 0.325,
+    "Pallet": 2.093,
+    "Pallet Large": 2.652,
+    "Pallet Medium Large": 1.7914,
+    "Pallet Medium Small": 1.443,
+    "Pallet Small Large": 0.9581,
+    "Pallet Small": 0.5902,
+    "Pallet Tall": 2.7274,
+    "Half Pallet": 1.0472,
+    "Tractor Trailer Load Floor": 52.00,
+    "Wall - Back": 12.116,
+    "Wall - Front": 4.4096,
+    "Pallite 16": 0.0537,
+    "Pallite 48": 0.0357
+}
